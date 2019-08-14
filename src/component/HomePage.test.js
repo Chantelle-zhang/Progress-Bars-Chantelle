6027c22ec18d2cc08506d355dfd720c2e64f8c76
chantelle <maudlin.nk@gmail.com>
2019-08-14 10:12:33 +1000

    Update HomePage.test.js

--- src/component/HomePage.test.js
+++ src/component/HomePage.test.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import {HomePage} from './HomePage';
 import {getPersonsDataWithLoadingState} from "../store/ActionCreators/actions";
+import Bars from "./Bars";
+import Select from "./Select";
+import ButtonGroup from "./ButtonGroup";
 
 
     it('Check Homepage componentDidMount()', () => {
@@ -15,4 +18,19 @@ import {getPersonsDataWithLoadingState} from "../store/ActionCreators/actions";
         expect(spy).toHaveBeenCalled;
     });
 
+
+it('Check Homepage component has one bars component,one Select component and one ButtonGroup component', () =>
+{
+    const props = {
+        getPersonsDataWithLoadingState
+    };
+    const wrapper = shallow(<HomePage {...props}/>)
+    const bar=wrapper.find(Bars)
+    const buttonGroup=wrapper.find(ButtonGroup)
+    const select=wrapper.find(Select)
+    expect(bar.length).toBe(1);
+    expect(buttonGroup.length).toBe(1);
+    expect(select.length).toBe(1);
+
+});
 
